32288f9cedcdae22bf4599bbaed166d0dfef781a
lamont <lamont@fastrobot.com>
2020-01-16 14:30:03 -0800

    shellcheck fixes, still a #patch

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,30 +4,33 @@ set -o
 
 function main {
   # Source the other files to gain access to their functions
-  scriptDir="$(dirname ${0})/scripts"
-  source ${scriptDir}/utils.sh
-  source ${scriptDir}/chef_exec_cookstyle.sh
-  source ${scriptDir}/chef_exec_foodcritic.sh
+  scriptDir="$(dirname "${0}")/scripts"
+  # shellcheck source=scripts/utils.sh
+  source "${scriptDir}/utils.sh"
+  # shellcheck source=scripts/chef_exec_cookstyle.sh
+  source "${scriptDir}/chef_exec_cookstyle.sh"
+  # shellcheck source=scripts/chef_exec_foodcritic.sh
+  source "${scriptDir}/chef_exec_foodcritic.sh"
 
   parseInputs
-  cd ${GITHUB_WORKSPACE}/${chefWorkingDir}
+  cd "${GITHUB_WORKSPACE}"/"${chefWorkingDir}"
 
   case "${chefTask}" in
     lint)
-      chefCookStyle ${*}
-      chefFoodcritic ${*}
+      chefCookStyle "$@"
+      chefFoodcritic "$@"
       ;;
     spec)
-      chefSpec ${*}
+      chefSpec "$@"
       ;;
     kitchen)
-      chefValidate ${*}
+      chefValidate "$@"
       ;;
     berks)
-      chefBerks ${*}
+      chefBerks "$@"
       ;;
     tag_and_publish)
-      chefTBD ${*}
+      chefTBD "$@"
       ;;
     *)
       echo "Error: Must provide a valid value for chef_task"
@@ -36,4 +39,4 @@ function main {
   esac
 }
 
-main "${*}"
+main "$@"
