8c544c499f270d792b464e260a71df80a4db0732
lamont <lamont@fastrobot.com>
2020-01-17 15:35:59 -0800

    #patch some debug in.

--- scripts/chef_exec_kitchen.sh
+++ scripts/chef_exec_kitchen.sh
@@ -2,8 +2,11 @@
 
 function chefValidate {
   echo "Running kitchen test"
+  chef gem list -l
+  kitchen diagnose --all
   chefOutput=$(chef exec kitchen test --destroy=always ${*} 2>&1)
   chefExitCode=${?}
+  cat .kitchen/logs/kitchen.log
 
   # kitchen exit 0 means all good
   if [ ${chefExitCode} -eq 0 ]; then
